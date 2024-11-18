e49370f1f15a2c41abc887fa7b88f094d37b04be
remotexman <149099948+remotexman@users.noreply.github.com>
2024-11-18 21:13:27 +0300

    Add files via upload

---
+++ section 7/module_7_1.py
@@ -0,0 +1,39 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        f = file.read()
+        file.close()
+        return f
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if product.name in self.get_products():
+                print(f'Продукт {product} уже есть в магазине')
+            else:
+                file.write(f'{product}\n')
+        file.close()
+
+if __name__ == '__main__':
+    s1 = Shop()
+    p1 = Product('Potato', 50.5, 'Vegetables')
+    p2 = Product('Spaghetti', 3.4, 'Groceries')
+    p3 = Product('Potato', 5.5, 'Vegetables')
+
+    print(p2)
+
+    s1.add(p1, p2, p3)
+
+    print(s1.get_products())
